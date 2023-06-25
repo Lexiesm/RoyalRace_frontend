fe8f28c47aba4f49441c1538adf8a38d10e4f7c9
IgnaciaGonzlez <ignacia.gonzlez@uc.cl>
2023-06-25 16:35:59 -0400

    Vista juego completada y alertas añadidas

--- ProyectoWeb/src/game/Tienda/botonTienda.jsx
+++ ProyectoWeb/src/game/Tienda/botonTienda.jsx
@@ -1,28 +1,46 @@
 import React, { useState } from "react";
 import './Tienda.css';
 import axios from 'axios';
+import PopUpAlerta from "../Alertas/Alertas";
 
 export default function BotonTienda({ id_player, tipo_objeto, handleClose }) {
-    const [data, setData] = useState(null);
+  const [data, setData] = useState(null);
+  const [showPopup, setShowPopup] = useState(false);
 
-    const handleClick = () => {
-        axios.post(`${import.meta.env.VITE_BACKEND_URL}/objects`, {
-            id_player: id_player,
-            tipo_objeto: tipo_objeto
-        })
-        .then(function (response) {
-            console.log(response.data);
-            setData(response.data);
-            handleClose();
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
-    }
+  const handleClick = () => {
+    axios.post(`${import.meta.env.VITE_BACKEND_URL}/objects`, {
+      id_player: id_player,
+      tipo_objeto: tipo_objeto
+    })
+    .then(function (response) {
+      console.log(response.data);
+      setData(response.data);
+      
+      if (response.data.error) {
+        setShowPopup(true);
+      } else {
+        handleClose();
+      }
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
 
-    return (
-        <button className="tienda-button" onClick={handleClick}>
-            Comprar
-        </button>
-    );
+  const handleTogglePopup = () => {
+    setShowPopup(!showPopup);
+  }
+
+  return (
+    <>
+      <button className="tienda-button" onClick={handleClick}>
+        Comprar
+      </button>
+      {data && data.error && (
+        <PopUpAlerta show={showPopup} handleClose={handleTogglePopup}>
+          {data.error}
+        </PopUpAlerta>
+      )}
+    </>
+  );
 }
